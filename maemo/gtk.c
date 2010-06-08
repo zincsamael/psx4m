b96501ca11bea3fd7626532e4ab22b1010f47926
Tomasz Sterna <tomek@xiaoka.com>
2010-06-08 17:42:34 +0200

    Stubbed video updates on Maemo

--- maemo/gtk.c
+++ maemo/gtk.c
@@ -1,18 +1,24 @@
 #include <unistd.h>
 #include "minimal.h"
 
-unsigned short _screenbuffer[320*240];
-unsigned short* screenbuffer = _screenbuffer;
+unsigned short* screenbuffer = NULL;
 
 void gp2x_change_res(int w, int h)
 {
 	printf("gp2x_change_res %dx%d\n", w, h);
+	if (screenbuffer) free(screenbuffer);
+	screenbuffer = (unsigned short*) malloc(w * h * sizeof(unsigned short));
 }
 
 unsigned long gp2x_joystick_read(void)
 {
 	printf("gp2x_joystick_read()\n");
 	return 0;
+}
+
+void gp2x_video_RGB_clearscreen16(void)
+{
+	printf("gp2x_video_RGB_clearscreen16()\n");
 }
 
 void updateScreen()
