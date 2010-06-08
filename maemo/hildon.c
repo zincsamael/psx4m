7db852af554eb2e2c39854c571c79821ca97b15c
Tomasz Sterna <tomek@xiaoka.com>
2010-06-09 00:40:44 +0200

    Use GdkImage for drawing

--- maemo/hildon.c
+++ maemo/hildon.c
@@ -5,7 +5,7 @@
 #include <hildon/hildon.h>
 #include "minimal.h"
 
-GdkPixbuf *pixbuf;
+GdkImage *image;
 HildonAnimationActor *actor;
 GtkWidget *window, *drawing;
 GdkGC *gc;
@@ -31,6 +31,8 @@ void hildon_init(int *argc, char ***argv)
 	gtk_init (argc, argv);
 
 	window = hildon_stackable_window_new ();
+	gtk_widget_realize (window);
+	gtk_window_fullscreen (GTK_WINDOW(window));
 //	actor = HILDON_ANIMATION_ACTOR (hildon_animation_actor_new());
 
 //	gtk_window_resize (GTK_WINDOW (actor), X_SIZE, Y_SIZE);
@@ -38,7 +40,7 @@ void hildon_init(int *argc, char ***argv)
 //	hildon_animation_actor_set_parent (actor, GTK_WINDOW (window));
 //	hildon_animation_actor_set_scale (actor, X_SCALE, Y_SCALE);
 
-	drawing = gtk_drawing_area_new ();
+	drawing = gtk_image_new ();
 	gc = gdk_gc_new (drawing->window);
 
 	gtk_container_add (GTK_CONTAINER (window), drawing);
@@ -52,14 +54,14 @@ void gp2x_change_res(int w, int h)
 {
 	printf("gp2x_change_res %dx%d\n", w, h);
 
-	if(w <= 0 || h <= 0)
+	if (w <= 0 || h <= 0)
 		return;
 
-	if (pixbuf) gdk_pixbuf_unref(pixbuf);
-	pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, w, h);
+	if (image) gdk_image_destroy(image);
+	image = gdk_image_new( GDK_IMAGE_FASTEST, gdk_visual_get_system(), w, h );
 
-	screenbuffer = (unsigned short*) gdk_pixbuf_get_pixels (pixbuf);
-	screen_size = w * h * ( gdk_pixbuf_get_bits_per_sample(pixbuf) / 8 );
+	screenbuffer = (unsigned short*) image->mem;
+	screen_size = w * h * image->bpp;
 }
 
 unsigned long gp2x_joystick_read(void)
@@ -73,15 +75,14 @@ unsigned long gp2x_joystick_read(void)
 
 void gp2x_video_RGB_clearscreen16(void)
 {
-	memset(gp2x_screen16, 0, screen_size);
+	if (gp2x_screen16 && screen_size)
+		memset(gp2x_screen16, 0, screen_size);
 }
 
 void updateScreen()
 {
-	gdk_draw_pixbuf(
-		drawing->window, NULL,
-		pixbuf, 0, 0, 0, 0,
-		gdk_pixbuf_get_width(pixbuf), gdk_pixbuf_get_height(pixbuf),
-		GDK_RGB_DITHER_NONE, 0, 0);
-	gdk_gc_unref(gc);
+	//gdk_draw_image(
+	//	drawing->window, gc,
+	//	image, 0, 0, 0, 0, -1, -1);
+	gtk_image_set_from_image (GTK_IMAGE(drawing), image, NULL);
 }
