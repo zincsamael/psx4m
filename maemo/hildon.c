6bbc2227ae6e5160c5e45b999dc439f374be4b7e
Tomasz Sterna <tomek@xiaoka.com>
2010-06-09 01:00:55 +0200

    Use HAA for scaling to 640x480

--- maemo/hildon.c
+++ maemo/hildon.c
@@ -8,21 +8,12 @@
 GdkImage *image;
 HildonAnimationActor *actor;
 GtkWidget *window, *drawing;
-GdkGC *gc;
-
-static gint X_SIZE = 320;
-static gint Y_SIZE = 240;
-
-static gint X_POS = 0;
-static gint Y_POS = 56;
-
-/* Using 800x424 as fullscreen */
-static gdouble X_SCALE = 2.5;  /* width / X_SIZE */
-static gdouble Y_SCALE = 1.77; /* height / Y_SIZE */
 
 #define X_RES           800
 #define Y_RES           480
-
+#define D_WIDTH			640
+#define D_HEIGHT		480
+		
 int screen_size;
 unsigned short* screenbuffer = NULL;
 
@@ -33,27 +24,21 @@ void hildon_init(int *argc, char ***argv)
 	window = hildon_stackable_window_new ();
 	gtk_widget_realize (window);
 	gtk_window_fullscreen (GTK_WINDOW(window));
-//	actor = HILDON_ANIMATION_ACTOR (hildon_animation_actor_new());
 
-//	gtk_window_resize (GTK_WINDOW (actor), X_SIZE, Y_SIZE);
-//	hildon_animation_actor_set_position (actor, X_POS, Y_POS);
-//	hildon_animation_actor_set_parent (actor, GTK_WINDOW (window));
-//	hildon_animation_actor_set_scale (actor, X_SCALE, Y_SCALE);
+	actor = HILDON_ANIMATION_ACTOR (hildon_animation_actor_new());
+	hildon_animation_actor_set_position (actor, (X_RES - D_WIDTH)/2, (Y_RES - D_HEIGHT)/2 );
+	hildon_animation_actor_set_parent (actor, GTK_WINDOW (window));
 
 	drawing = gtk_image_new ();
-	gc = gdk_gc_new (drawing->window);
 
-	gtk_container_add (GTK_CONTAINER (window), drawing);
-//	gtk_container_add (GTK_CONTAINER (actor), drawing);
+	gtk_container_add (GTK_CONTAINER (actor), drawing);
 
-//	gtk_widget_show_all (GTK_WIDGET (actor));
+	gtk_widget_show_all (GTK_WIDGET (actor));
 	gtk_widget_show_all (GTK_WIDGET (window));
 }
 
 void gp2x_change_res(int w, int h)
 {
-	printf("gp2x_change_res %dx%d\n", w, h);
-
 	if (w <= 0 || h <= 0)
 		return;
 
@@ -64,6 +49,12 @@ void gp2x_change_res(int w, int h)
 	screen_size = w * h * image->bpp;
 
 	gtk_image_set_from_image (GTK_IMAGE(drawing), image, NULL);
+
+	gtk_window_resize (GTK_WINDOW (actor), w, h);
+	hildon_animation_actor_set_scale (actor,
+				(gdouble)D_WIDTH / (gdouble)w,
+				(gdouble)D_HEIGHT / (gdouble)h
+	);
 }
 
 unsigned long gp2x_joystick_read(void)
