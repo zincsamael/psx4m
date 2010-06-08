b8302fdd7d5455e37e8f7eb261a68cf1e67b36a7
Tomasz Sterna <tomek@xiaoka.com>
2010-06-08 18:18:54 +0200

    Renamed maemo/gtk.c to hildon.c

---
+++ maemo/hildon.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "minimal.h"
+
+unsigned short* screenbuffer = NULL;
+
+void hildon_init()
+{
+}
+
+void gp2x_change_res(int w, int h)
+{
+	printf("gp2x_change_res %dx%d\n", w, h);
+	if (screenbuffer) free(screenbuffer);
+	screenbuffer = (unsigned short*) malloc(w * h * sizeof(unsigned short));
+}
+
+unsigned long gp2x_joystick_read(void)
+{
+	printf("gp2x_joystick_read()\n");
+	return 0;
+}
+
+void gp2x_video_RGB_clearscreen16(void)
+{
+	printf("gp2x_video_RGB_clearscreen16()\n");
+}
+
+void updateScreen()
+{
+	printf("updateScreen()\n");
+}
